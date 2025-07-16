f0246565a9e023242d1ce225780844cb5c77ba78
psyq <73028291+psyq55262227@users.noreply.github.com>
2025-07-16 16:00:53 +0800

    fix: cache add error in ai bot

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,23 @@ import MillionLint from '@million/lint';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+        ],
+      },
+    ];
+  },
   webpack(config, { isServer }) {
     const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
     config.module.rules = [
