acc29c602a1b64f38864a29b68e2776084808dd1
yusixian <i@cosine.ren>
2025-07-18 16:40:07 +0800

    feat: update EntryChart and EntryInstanceList

--- src/components/entry/EntryPageContent.tsx
+++ src/components/entry/EntryPageContent.tsx
@@ -1,13 +1,13 @@
 'use client';
+import { entryInstancesMapAtom } from '@/atoms';
 import EntryChart from '@/components/entry/EntryChart';
 import EntryInstanceList from '@/components/entry/EntryInstanceList';
 import EntryTypeListForCompletion from '@/components/entry/EntryTypeListForCompletion';
-import EntryHeader from './EntryHeader';
-import { useRef, useLayoutEffect, useCallback } from 'react';
-import { useAtomValue } from 'jotai';
-import { entryInstancesMapAtom } from '@/atoms';
-import dayjs from 'dayjs';
 import { usePrevious } from '@/hooks/usePrevious';
+import dayjs from 'dayjs';
+import { useAtomValue } from 'jotai';
+import { useCallback, useLayoutEffect, useRef } from 'react';
+import EntryHeader from './EntryHeader';
 
 export default function EntryPageContent() {
   const scrollContainerRef = useRef<HTMLDivElement>(null);
@@ -43,7 +43,7 @@ export default function EntryPageContent() {
     <div ref={scrollContainerRef} className="flex h-full flex-col gap-3 overflow-auto px-4 pb-40 text-center">
       <EntryHeader />
       <EntryChart />
-      <EntryInstanceList />
+      <EntryInstanceList className="-mt-18" />
       <EntryTypeListForCompletion />
     </div>
   );
